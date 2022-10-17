2569512b2685b311822dad87217d88db66f147a3
Zyrerin <jordy.van.de.laar@student.gildeopleidingen.nl>
2022-10-17 13:41:13 +0200

    Added a multiplier upgrade which doubles earnings

--- functions.js
+++ functions.js
@@ -12,8 +12,14 @@ class Upgrade {
     get buyUpgrade() {
         if(defenestrates >= this.cost) {
             defenestrates -= this.cost;
-            this.effect = this.effect * 1.1; // 10% effect increase per upgrade
-            this.cost = this.cost * 1.2; // 20% price increase per upgrade
+            if(this.name == "Worth Upgrade") { // Worth Upgrade
+                this.effect = this.effect * 1.1; // 10% effect increase per upgrade
+                this.cost = this.cost * 1.2; // 20% price increase per upgrade
+            }else if(this.name == "Double Upgrade")
+            { // Double Upgrade
+                this.effect = this.effect * 2; // 100% effect increase per upgrade
+                this.cost = this.cost * 2.5; // 150% price increase per upgrade
+            }
             updateDefenestrates();
         }
     }
@@ -30,24 +36,25 @@ class Upgrade {
 //functions
 
 var defenestrates = 0; // the amount of currency
-var worthUpgrade = new Upgrade("Worth Upgrade", 10); // make a worth upgrade base cost 10
+var worthUpgrade = new Upgrade("Worth Upgrade", 10); // make a worth upgrade, base cost 10
+var doubleUpgrade = new Upgrade("Double Upgrade", 100); // make a double upgrade, base cost 100
 
 function updateDefenestrates() { // updating data
     document.getElementById("defenestratesText").innerHTML = defenestrates.toFixed(1); // change the text to the amount of defenestrates
-    document.getElementById("defenestrateBtn").innerHTML = "Defenestrate: " + worthUpgrade.getEffect.toFixed(1) + " gained"; // change the text to the amount of defenestrates gained
+    document.getElementById("defenestrateBtn").innerHTML = "Defenestrate: " + (worthUpgrade.getEffect*doubleUpgrade.getEffect).toFixed(1) + " gained"; // change the text to the amount of defenestrates gained
 }
 
 function doDefenestrate() { // commit a felony
-    defenestrates += worthUpgrade.getEffect; // add the effect of the worth upgrade
+    defenestrates += (worthUpgrade.getEffect * doubleUpgrade.effect); // add the effect of the worth upgrade
     updateDefenestrates();
 }
 
 function doWorthUpgrade() { // buy an upgrade
     worthUpgrade.buyUpgrade;
-document.getElementById("Upgrade").innerHTML = worthUpgrade.getName + ": " + Math.floor(worthUpgrade.cost); // change the text to the name of the upgrade and the cost
+    document.getElementById("WorthUpgrade").innerHTML = worthUpgrade.getName + ": " + Math.floor(worthUpgrade.cost); // change the text to the name of the upgrade and the cost
 }
 
-
-
-
-
+function doDoubleUpgrade(){
+    doubleUpgrade.buyUpgrade;
+    document.getElementById("DoubleUpgrade").innerHTML =  "Double Earnings: " + Math.floor(doubleUpgrade.cost);
+}
